e3a6b7a745200b2146c5dfd4fefa28846c8a1d35
Friendly Alien <164930880+fryalien@users.noreply.github.com>
2024-11-10 12:03:02 +0100

    y

--- fish/config.fish
+++ fish/config.fish
@@ -18,32 +18,19 @@ set fish_color_valid_path
 #-------------------------
 # ALIASES
 #-------------------------
-
-### NIXOS GENERAL
-# list active channel
-abbr lsch 'sudo nix-channel --list'
-# delete old stuff
-abbr clean 'sudo nix-collect-garbage --delete-older-than 3d'
-# look when something is being deleted
-abbr lst 'systemctl list-timers'
-# list generations
-abbr lsgen 'sudo nix-env --list-generations --profile /nix/var/nix/profiles/system'
-
-### WITHOUT FLAKES
-#abbr up 'sudo nix-channel --update'
-#rs 'sudo nixos-rebuild switch'
-
-### WITH FLAKES
-# update nix flake, has to be in the directory of the flake.lock file
-#abbr up 'cd ~/nixcfg; nix flake update'
-# update the system
-#abbr rs 'sudo nixos-rebuild switch --flake ~/nixcfg'
-
-### WITH FALKES AND HOME MANAGER
-abbr up 'cd ~/nixcfg; nix flake update'
-# UPDATE SYSTEM
-abbr rs  'sudo nixos-rebuild switch --flake ~/nixcfg'
-abbr rhm 'home-manager switch --flake ~/nixcfg'
+# INSTALL PACKAGES
+abbr u 'sudo pacman -Syu'
+abbr i 'sudo pacman -S'
+abbr pkr 'sudo pacman -Rs'
+abbr pks 'sudo pacman -Ss'
+abbr f 'flatpak install'
+abbr fu 'flatpak update'
+abbr fr 'flatpak remove'
+abbr fs 'flatpak search'
+abbr fcl 'flatpak uninstall --unused'
+abbr fls 'flatpak list'
+abbr yi 'yay -S'
+abbr yup 'yay -Syu'
 
 # CLI COMMANDS
 alias l 'eza -lh --group-directories-first'
@@ -52,11 +39,24 @@ abbr t 'btop'
 abbr ri 'rg -i'
 abbr s 'sudo'
 abbr d 'du -sh *'
+abbr sfi 'source ~/.config/fish/config.fish'
+
+# INSTALLED
+abbr pla 'pacman -Qe > ~/Downloads/pacman-all-list.md'
+abbr fl 'flatpak list > ~/Downloads/flatpak-list.md'
 
 # SHORCUTS FOR PROGRAMS
 abbr v 'nvim'
 abbr V 'sudo nvim'
+abbr r 'ranger'
 abbr e 'micro'
+
+# PLACES
+abbr dw 'cd ~/Downloads'
+abbr dc 'cd ~/Documents'
+abbr pc 'cd ~/Pictures'
+abbr vd 'cd ~/Videos'
+abbr gh 'cd ~/BIGDATA/GitHub'
 
 # POWER
 abbr sd 'shutdown now'
@@ -66,8 +66,45 @@ abbr re 'reboot'
 abbr 4k 'xrandr -s 3840x2160'
 abbr hd 'xrandr -s 1920x1080'
 
+# MELD CONFIG
+abbr fic 'meld ~/.config/fish/config.fish ~/BIGDATA/GitHub/dotfiles/fish/config.fish &; disown'
+abbr i3c 'meld ~/.config/i3/config ~/BIGDATA/GitHub/dotfiles/i3/config &; disown'
+abbr i3s 'meld ~/.config/i3/i3status.conf ~/BIGDATA/GitHub/config/i3/i3status.conf &; disown'
+
+# SOURCE CONFIG FILES
+abbr fis 'source ~/.config/fish/config.fish'
+abbr zshs 'source ~/.zshrc'
+abbr bas ' source ~/.bashrc'
+
 # YOUTUBE DOWNLOAD
 abbr yab 'cd ~/Downloads/; yt-dlp -x --audio-format mp3 -f bestaudio '
 abbr ya 'cd ~/Downloads/; yt-dlp --extract-audio --audio-format mp3 --audio-quality 0 '
 abbr yv 'cd ~/Downloads/; yt-dlp '
 
+# PYTHON3 SERVER
+abbr py 'python3 -m http.server -b '
+abbr ip 'hostname -I'
+abbr web '~/.local/bin/httpserv.sh'
+
+# git
+abbr gc 'cd /BIGDATA/git; git clone'
+
+#mygit config
+abbr gdot 'git clone https://github.com/fryalien/dotfiles'
+
+# PACMAN clean packages
+abbr pkcl 'sudo paccache -r'
+
+# LOCKED PACMAN DATABASE
+# check if something is holding the lock
+abbr lck 'lsof /var/lib/pacman/db.lck'
+# remove lock
+abbr rl 'rm /var/lib/pacman/db.lck'
+
+# FZF settings
+fzf --fish | source
+
+export FZF_DEFAULT_OPTS="--preview 'bat --color=always --style=numbers {}' --bind shift-up:preview-page-up,shift-down:preview-page-down"
+export FZF_DEFAULT_COMMAND="fd --type f"
+export FZF_CTRL_T_OPTS="--preview='less {}' --height=100% --bind shift-up:preview-page-up,shift-down:preview-page-down"
+export FZF_COMPLETION_DIR_COMMANDS="cd pushd rmdir tree"
