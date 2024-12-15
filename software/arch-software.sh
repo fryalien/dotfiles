429aa7352bbd30d4bab3ff1c5ddfe29d89065aa6
fryalien <164930880+fryalien@users.noreply.github.com>
2024-12-15 18:59:45 +0100

    y

--- software/arch-software.sh
+++ software/arch-software.sh
@@ -1,15 +1,5 @@
 # INSTALL PACKAGES
 
-#paru -S --noconfirm --needed parabolic-git
-paru -S --noconfirm --needed freefilesync-bin
-paru -S --noconfirm --needed freetube-bin
-paru -S --noconfirm --needed github-desktop-bin
-paru -S --noconfirm --needed localsend-bin
-paru -S --noconfirm --needed megasync-bin
-paru -S --noconfirm --needed rustdesk-bin
-paru -S --noconfirm --needed syncthing-gtk
-paru -S --noconfirm --needed ungoogled-chromium
-sudo pacman -S --noconfirm --needed brave-bin
 sudo pacman -S --noconfirm --needed calibre
 sudo pacman -S --noconfirm --needed eza
 sudo pacman -S --noconfirm --needed fastfetch
@@ -19,21 +9,16 @@ sudo pacman -S --noconfirm --needed gparted
 sudo pacman -S --noconfirm --needed gzip
 sudo pacman -S --noconfirm --needed imagemagick
 sudo pacman -S --noconfirm --needed kbackup
-sudo pacman -S --noconfirm --needed keepassxc
-sudo pacman -S --noconfirm --needed librewolf-bin
 sudo pacman -S --noconfirm --needed meld
 sudo pacman -S --noconfirm --needed micro
 sudo pacman -S --noconfirm --needed mpv
 sudo pacman -S --noconfirm --needed nextcloud-client
 sudo pacman -S --noconfirm --needed obs-studio
-sudo pacman -S --noconfirm --needed obsidian
 sudo pacman -S --noconfirm --needed onlyoffice-bin
 sudo pacman -S --noconfirm --needed p7zip
-sudo pacman -S --noconfirm --needed qbittorrent
 sudo pacman -S --noconfirm --needed rate-mirrors-bin
 sudo pacman -S --noconfirm --needed ripgrep
 sudo pacman -S --noconfirm --needed rsync
-sudo pacman -S --noconfirm --needed signal-desktop
 sudo pacman -S --noconfirm --needed spotify
 sudo pacman -S --noconfirm --needed tenacity
 sudo pacman -S --noconfirm --needed unace
@@ -43,9 +28,13 @@ sudo pacman -S --noconfirm --needed vivaldi
 sudo pacman -S --noconfirm --needed vivaldi-ffmpeg-codecs
 sudo pacman -S --noconfirm --needed vivaldi-widevine
 sudo pacman -S --noconfirm --needed vlc
-sudo pacman -S --noconfirm --needed vscodium
 sudo pacman -S --noconfirm --needed wireshark-qt
 sudo pacman -S --noconfirm --needed yt-dlp
+
+paru -S --noconfirm --needed freefilesync-bin
+paru -S --noconfirm --needed localsend-bin
+paru -S --noconfirm --needed megasync-bin
+paru -S --noconfirm --needed rustdesk-bin
 
 # THUNAR
 sudo pacman -S --noconfirm --needed thunar
@@ -53,3 +42,27 @@ sudo pacman -S --noconfirm --needed thunar-archive-plugin
 sudo pacman -S --noconfirm --needed thunar-volman
 
 # FLATPACKS
+#paru -S --noconfirm --needed freetube-bin
+#paru -S --noconfirm --needed github-desktop-bin
+#paru -S --noconfirm --needed parabolic-git
+#paru -S --noconfirm --needed syncthing-gtk
+#paru -S --noconfirm --needed ungoogled-chromium
+#sudo pacman -S --noconfirm --needed brave-bin
+#sudo pacman -S --noconfirm --needed keepassxc
+#sudo pacman -S --noconfirm --needed librewolf-bin
+#sudo pacman -S --noconfirm --needed obsidian
+#sudo pacman -S --noconfirm --needed qbittorrent
+#sudo pacman -S --noconfirm --needed signal-desktop
+#sudo pacman -S --noconfirm --needed vscodium
+flatpak install -y com.brave.Browser
+flatpak install -y com.vscodium.codium
+flatpak install -y io.freetubeapp.FreeTube
+flatpak install -y io.github.shiftey.Desktop #GitHub Desktop
+flatpak install -y io.github.ungoogled_software.ungoogled_chromium
+flatpak install -y io.gitlab.librewolf-community
+flatpak install -y md.obsidian.Obsidian
+flatpak install -y me.kozec.syncthingtk
+flatpak install -y org.keepassxc.KeePassXC
+flatpak install -y org.nickvision.tubeconverter
+flatpak install -y org.qbittorrent.qBittorrent
+flatpak install -y org.signal.Signal
