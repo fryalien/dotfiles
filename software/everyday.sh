70359eab9754e1d46a6f1d35569933baad473b45
Friendly Alien <164930880+fryalien@users.noreply.github.com>
2025-02-06 10:55:15 +0100

    y

--- software/everyday.sh
+++ software/everyday.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-pkmgr="apt install -y"
+pkmgr="pacman -S --noconfirm --needed"
+
+# ARCO "pacman -S --noconfirm --needed"
+# FEDORA "dnf -y"
+# KUBUNTU "apt install -y"
 
 sudo $pkmgr fastfetch
 sudo $pkmgr wireshark
@@ -14,7 +18,7 @@ sudo $pkmgr bat
 sudo $pkmgr eza
 sudo $pkmgr ripgrep
 sudo $pkmgr yt-dlp
-sudo $pkmgr fd
+sudo $pkmgr fd-find
 
 flatpak install -y com.github.tchx84.Flatseal
 flatpak install -y org.onlyoffice.desktopeditors
@@ -37,15 +41,15 @@ flatpak install -y com.calibre_ebook.calibre
 flatpak install -y org.kde.tokodon
 flatpak install -y org.gnome.meld
 flatpak install -y org.localsend.localsend_app
+flatpak install -y io.github.flattool.Warehouse
 
 
-#freefilesync
+### freefilesync ###
 #https://freefilesync.org/download.php
 
-#MEGASYNC
-
-#ubuntu
-wget https://mega.nz/linux/repo/xUbuntu_24.10/amd64/megasync-xUbuntu_24.10_amd64.deb && sudo apt install "$PWD/megasync-xUbuntu_24.10_amd64.deb"
+### MEGASYNC ###
+#kubuntu
+#wget https://mega.nz/linux/repo/xUbuntu_24.10/amd64/megasync-xUbuntu_24.10_amd64.deb && sudo apt install "$PWD/megasync-xUbuntu_24.10_amd64.deb"
 
 #arco
-#sudo pacman -S magasync-bin
+sudo pacman -S --noconfirm --needed magasync-bin
