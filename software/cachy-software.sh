6afd3966a6ae148941a6098c715e655fca710196
Friendly Alien <164930880+fryalien@users.noreply.github.com>
2025-04-23 14:47:24 +0200

    y

--- software/cachy-software.sh
+++ software/cachy-software.sh
@@ -21,10 +21,12 @@ sudo pacman -S --noconfirm --needed nextcloud-client
 sudo pacman -S --noconfirm --needed obs-studio
 sudo pacman -S --noconfirm --needed onlyoffice-bin
 sudo pacman -S --noconfirm --needed p7zip
+sudo pacman -S --noconfirm --needed partitionmanager
 sudo pacman -S --noconfirm --needed rate-mirrors-bin
 sudo pacman -S --noconfirm --needed ripgrep
 sudo pacman -S --noconfirm --needed rsync
 sudo pacman -S --noconfirm --needed tenacity
+sudo pacman -S --noconfirm --needed tokodon
 sudo pacman -S --noconfirm --needed ufw gufw
 sudo pacman -S --noconfirm --needed unace
 sudo pacman -S --noconfirm --needed unrar
@@ -45,15 +47,16 @@ sudo pacman -S --noconfirm --needed localsend
 sudo pacman -S --noconfirm --needed obsidian
 sudo pacman -S --noconfirm --needed qbittorrent
 sudo pacman -S --noconfirm --needed signal-desktop
-sudo pacman -S --noconfirm --needed spotify
+sudo pacman -S --noconfirm --needed varia
+sudo pacman -S --noconfirm --needed zen-browser-bin
 sudo pacman -S --noconfirm --needed vscodium
 
 # AUR
 paru -S --noconfirm --needed github-desktop-bin
 paru -S --noconfirm --needed megasync-bin
 paru -S --noconfirm --needed rustdesk-bin
+paru -S --noconfirm --needed spotify
 paru -S --noconfirm --needed ungoogled-chromium-bin
 
 #NON BIN
 paru -S --noconfirm --needed parabolic-qt
-
