f5f592e9f0dd8a1c6494579ba6643fb97694d55a
Friendly Alien <164930880+fryalien@users.noreply.github.com>
2025-04-23 13:29:02 +0200

    y

---
+++ software/cachy-software.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# INSTALL PACKAGES
+sudo pacman -S --noconfirm --needed calibre
+sudo pacman -S --noconfirm --needed dos2unix
+sudo pacman -S --noconfirm --needed eza
+sudo pacman -S --noconfirm --needed fastfetch
+sudo pacman -S --noconfirm --needed filelight
+sudo pacman -S --noconfirm --needed fish
+sudo pacman -S --noconfirm --needed fzf
+sudo pacman -S --noconfirm --needed gparted
+sudo pacman -S --noconfirm --needed gzip
+sudo pacman -S --noconfirm --needed imagemagick
+sudo pacman -S --noconfirm --needed kbackup
+sudo pacman -S --noconfirm --needed kcalc
+sudo pacman -S --noconfirm --needed keepassxc
+sudo pacman -S --noconfirm --needed meld
+sudo pacman -S --noconfirm --needed micro
+sudo pacman -S --noconfirm --needed mpv
+sudo pacman -S --noconfirm --needed nextcloud-client
+sudo pacman -S --noconfirm --needed obs-studio
+sudo pacman -S --noconfirm --needed onlyoffice-bin
+sudo pacman -S --noconfirm --needed p7zip
+sudo pacman -S --noconfirm --needed rate-mirrors-bin
+sudo pacman -S --noconfirm --needed ripgrep
+sudo pacman -S --noconfirm --needed rsync
+sudo pacman -S --noconfirm --needed tenacity
+sudo pacman -S --noconfirm --needed ufw gufw
+sudo pacman -S --noconfirm --needed unace
+sudo pacman -S --noconfirm --needed unrar
+sudo pacman -S --noconfirm --needed unzip
+sudo pacman -S --noconfirm --needed vivaldi
+sudo pacman -S --noconfirm --needed vivaldi-ffmpeg-codecs
+sudo pacman -S --noconfirm --needed vivaldi-widevine
+sudo pacman -S --noconfirm --needed vlc
+sudo pacman -S --noconfirm --needed wireshark-qt
+sudo pacman -S --noconfirm --needed yt-dlp
+
+# EXTRA
+sudo pacman -S --noconfirm --needed brave-bin
+sudo pacman -S --noconfirm --needed freefilesync
+sudo pacman -S --noconfirm --needed freetube-bin
+sudo pacman -S --noconfirm --needed librewolf-bin
+sudo pacman -S --noconfirm --needed localsend
+sudo pacman -S --noconfirm --needed obsidian
+sudo pacman -S --noconfirm --needed qbittorrent
+sudo pacman -S --noconfirm --needed signal-desktop
+sudo pacman -S --noconfirm --needed spotify
+sudo pacman -S --noconfirm --needed vscodium
+
+# AUR
+paru -S --noconfirm --needed github-desktop-bin
+paru -S --noconfirm --needed megasync-bin
+paru -S --noconfirm --needed rustdesk-bin
+paru -S --noconfirm --needed ungoogled-chromium-bin
+
+#NON BIN
+paru -S --noconfirm --needed parabolic-qt
+
