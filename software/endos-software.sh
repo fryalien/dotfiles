b8dd116a7730dca6e90fb168cfd349f1767f7947
Friendly Alien <164930880+fryalien@users.noreply.github.com>
2026-01-19 18:23:48 +0100

    y

--- software/endos-software.sh
+++ software/endos-software.sh
@@ -6,47 +6,55 @@ sudo pacman -Rns --noconfirm firefox
 sudo pacman -Rns --noconfirm firewalld
 sudo pacman -Rns --noconfirm meld
 
-pkmgr="pacman -S --noconfirm --needed"
+in="sudo pacman -S --noconfirm --needed"
 
 # INSTALL PACKAGES
-sudo $pkmgr bat
-sudo $pkmgr btop
-sudo $pkmgr dos2unix
-sudo $pkmgr eza
-sudo $pkmgr fastfetch
-sudo $pkmgr fd
-sudo $pkmgr ffmpeg
-sudo $pkmgr filelight
-sudo $pkmgr fish
-sudo $pkmgr fzf
-sudo $pkmgr git
-sudo $pkmgr gparted
-sudo $pkmgr gzip
-sudo $pkmgr imagemagick
-sudo $pkmgr kbackup
-sudo $pkmgr kcalc
-sudo $pkmgr micro
-sudo $pkmgr ncdu
-sudo $pkmgr neovim
-sudo $pkmgr p7zip
-sudo $pkmgr partitionmanager
-sudo $pkmgr rate-mirrors
-sudo $pkmgr ripgrep
-sudo $pkmgr rsync
-sudo $pkmgr tldr
-sudo $pkmgr tmux
-sudo $pkmgr ufw gufw
-sudo $pkmgr unace
-sudo $pkmgr unrar
-sudo $pkmgr unzip
-sudo $pkmgr wireshark-qt
-sudo $pkmgr yt-dlp
+$in bat
+$in btop
+$in dos2unix
+$in eza
+$in fastfetch
+$in fd
+$in ffmpeg
+$in filelight
+$in fish
+$in flatpak
+$in fzf
+$in git
+$in gparted
+$in gzip
+$in imagemagick
+$in kbackup
+$in kcalc
+$in micro
+$in ncdu
+$in neovim
+$in p7zip
+$in partitionmanager
+$in rate-mirrors
+$in ripgrep
+$in rsync
+$in tldr
+$in tmux
+$in ufw gufw
+$in unace
+$in unrar
+$in unzip
+$in wireshark-qt
+$in yt-dlp
+$in zoxide
 
 # AUR
-yay -S --noconfirm --needed flatpak
-yay -S --noconfirm --needed megasync-bin
-yay -S --noconfirm --needed freefilesync-bin
-yay -S --noconfirm --needed ttf-ms-fonts
+aur="yay -S --noconfirm --needed"
+
+$aur megasync-bin
+$aur freefilesync-bin
+$aur ttf-ms-fonts
+$aur catppuccin-cursors-latte
+$aur catppuccin-cursors-frappe
+$aur catppuccin-cursors-macchiato
+$aur catppuccin-cursors-mocha
+$aur paru
 
 # Add flathub remote.
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
