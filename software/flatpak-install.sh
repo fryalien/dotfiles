3f704b35020c0a4068b1528a2c75c45fd63cf58a
Friendly Alien <164930880+fryalien@users.noreply.github.com>
2026-01-24 12:35:27 +0100

    y

--- software/flatpak-install.sh
+++ software/flatpak-install.sh
@@ -5,16 +5,9 @@ flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.f
 
 fi="flatpak install flathub -y"
 
-#$fi me.kozec.syncthingtk
-#$fi nz.mega.MEGAsync
-#$fi org.freefilesync.FreeFileSync
-#$fi org.kde.kdenlive
-#$fi org.wireshark.Wireshark
-#$fi uk.org.greenend.chiark.sgtatham.putty
 $fi com.bilingify.readest
 $fi com.brave.Browser
 $fi com.calibre_ebook.calibre
-$fi com.discordapp.Discord
 $fi com.github.debauchee.barrier
 $fi com.github.johnfactotum.Foliate
 $fi com.github.tchx84.Flatseal
@@ -23,13 +16,12 @@ $fi com.rustdesk.RustDesk
 $fi com.vivaldi.Vivaldi
 $fi com.vscodium.codium
 $fi eu.betterbird.Betterbird
-$fi flathub org.gnome.Showtime
+$fi org.gnome.Showtime #Video player
 $fi io.freetubeapp.FreeTube
 $fi io.github.ellie_commons.cherrypick #Color picker
 $fi io.github.flattool.Warehouse
 $fi io.github.giantpinkrobots.varia
 $fi io.github.shiftey.Desktop #GitHub Desktop
-$fi io.github.ungoogled_software.ungoogled_chromium
 $fi io.mpv.Mpv
 $fi md.obsidian.Obsidian
 $fi no.mifi.losslesscut
@@ -38,8 +30,6 @@ $fi org.flameshot.Flameshot
 $fi org.geany.Geany
 $fi org.gnome.Lollypop
 $fi org.gnome.meld
-$fi org.kde.filelight
-$fi org.kde.kwrite
 $fi org.kde.tokodon
 $fi org.keepassxc.KeePassXC
 $fi org.localsend.localsend_app
@@ -55,5 +45,16 @@ $fi com.github.tenderowl.frog
 
 # When KDE is not installed.
 #$fi org.kde.dolphin
+#$fi org.kde.filelight
 #$fi org.kde.kwrite
 #$fi org.kde.KStyle.Kvantum
+
+#Not prefered.
+#$fi io.github.ungoogled_software.ungoogled_chromium
+#$fi me.kozec.syncthingtk
+#$fi nz.mega.MEGAsync
+#$fi org.freefilesync.FreeFileSync
+#$fi org.kde.kdenlive
+#$fi org.wireshark.Wireshark
+#$fi uk.org.greenend.chiark.sgtatham.putty
+#$fi com.discordapp.Discord
