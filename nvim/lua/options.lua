d642b8301366e151787cf728ef133afda4df0f5a
Friendly Alien <164930880+fryalien@users.noreply.github.com>
2026-02-07 07:39:07 +0100

    y

--- nvim/lua/options.lua
+++
@@ -1,6 +0,0 @@
--- Editor options
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.cursorline = true   -- highlight current line
-vim.opt.signcolumn = "yes"  -- keep sign column visible
-vim.opt.wrap = true
